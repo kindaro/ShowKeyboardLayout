3125e5ae36c0bb0a63158eaf53fc69e573cc39a9
Ignat Insarov <kindaro@gmail.com>
2017-01-17 04:44:02 +0300

    More C code for X11.

--- example.c
+++ example.c
@@ -1,9 +1,66 @@
 #include <stdio.h>
 #include <X11/XKBlib.h>
+#include <unistd.h>
+
+void init_x();
+
+Display *dis;
+int screen;
+Window win;
+GC gc;
 
 int main ()
 {
     printf("lala");
     printf("%i", XkbOpenDisplay("lalafa",0,0,0,0,0));
     printf("fa\n");
+    init_x();
+    printf("X system initialized!");
 }
+
+void init_x() {
+	/* get the colors black and white (see section for details) */
+	unsigned long black,white;
+
+	/* use the information from the environment variable DISPLAY 
+	   to create the X connection:
+	*/	
+	dis=XOpenDisplay(":0");
+   	screen=DefaultScreen(dis);
+	black=BlackPixel(dis,screen),	/* get color black */
+	white=WhitePixel(dis, screen);  /* get color white */
+
+	/* once the display is initialized, create the window.
+	   This window will be have be 200 pixels across and 300 down.
+	   It will have the foreground white and background black
+	*/
+   	win=XCreateSimpleWindow(dis,DefaultRootWindow(dis),0,0,	
+		200, 300, 5, white, black);
+
+	/* here is where some properties of the window can be set.
+	   The third and fourth items indicate the name which appears
+	   at the top of the window and the name of the minimized window
+	   respectively.
+	*/
+	/* XSetStandardProperties(dis,win,"My Window","HI!",None,NULL,0,NULL); */
+
+	/* this routine determines which types of input are allowed in
+	   the input.  see the appropriate section for details...
+	*/
+	XSelectInput(dis, win, ExposureMask|ButtonPressMask|KeyPressMask);
+
+	/* create the Graphics Context */
+        gc=XCreateGC(dis, win, 0,0);        
+
+	/* here is another routine to set the foreground and background
+	   colors _currently_ in use in the window.
+	*/
+	XSetBackground(dis,gc,white);
+	XSetForeground(dis,gc,black);
+
+	/* clear the window and bring it on top of the other windows */
+	XClearWindow(dis, win);
+	XMapRaised(dis, win);
+	sleep(10);
+};
+
