0cbf5a9adc2ad1c2fb904f9d90c713bec17280d2
Ignat Insarov <kindaro@gmail.com>
2017-01-17 05:49:09 +0300

    FFI XkbOpenDisplay.

--- ShowKeyboardLayout.hs
+++ ShowKeyboardLayout.hs
@@ -7,6 +7,45 @@
 
 module Main where
 
-main = putStrLn . show $ c_exp 13
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+import Foreign.Storable
+import System.IO.Unsafe
 
-foreign import ccall "exp" c_exp :: Double -> Double
+type Display = Int -- cz idk
+
+main = putStrLn . show $ openDisplay
+
+openDisplay = unsafePerformIO $
+    alloca $ \display_name -> alloca $ \event_rtrn -> alloca $ \error_rtrn ->
+        alloca $ \major_in_out -> alloca $ \minor_in_out -> alloca $ \reason_rtrn -> do
+            poke display_name nullPtr
+            poke major_in_out 1
+            poke minor_in_out 0
+            dpy <- c_XkbOpenDisplay display_name event_rtrn error_rtrn
+                                        major_in_out minor_in_out reason_rtrn
+            event_rtrn_peek <- peek event_rtrn
+            error_rtrn_peek <- peek error_rtrn
+            major_in_out_peek <- peek major_in_out
+            minor_in_out_peek <- peek minor_in_out
+            reason_rtrn_peek <- peek reason_rtrn
+            return ( dpy
+                   , event_rtrn_peek
+                   , error_rtrn_peek
+                   , major_in_out_peek
+                   , minor_in_out_peek
+                   , reason_rtrn_peek
+                   )
+
+--  Display * XkbOpenDisplay
+--      (display_name, event_rtrn, error_rtrn, major_in_out, minor_in_out, reason_rtrn)
+foreign import ccall unsafe "XkbOpenDisplay"
+    c_XkbOpenDisplay :: Ptr CString -- hardware display name
+                     -> Ptr CInt -- backfilled with the extension base event code
+                     -> Ptr CInt -- backfilled with the extension base error code
+                     -> Ptr CInt -- compile time lib major version in, server major version out
+                     -> Ptr CInt -- compile time lib minor version in, server minor version out
+                     -> Ptr CInt -- backfilled with a status code
+                     -> IO (Ptr Display)
