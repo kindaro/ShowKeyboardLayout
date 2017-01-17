5fb1389339e7bcb945a205acd1d1f91fe6394382
Ignat Insarov <kindaro@gmail.com>
2017-01-17 03:33:21 +0300

    Working FFI example.

--- ShowKeyboardLayout.hs
+++ ShowKeyboardLayout.hs
@@ -3,46 +3,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 
 module Main where
 
-import Control.Arrow
+main = putStrLn . show $ c_exp 13
 
-import Shelly
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-default (T.Text)
-
-main :: IO ()
-main = do
-    values <- setxkbmap
-    key <- xset
-    TIO.putStrLn $ values !! key
-
-xset :: IO Int
-xset = fmap parse output
-    where
-
-        output :: IO T.Text
-        output = shelly . silently $ run "xset" ["-q"]
-
-        parse :: T.Text -> Int
-        parse = T.lines
-            >>> filter (T.isInfixOf "LED") >>> head
-            >>> T.words >>> (!! 9) >>> flip T.index 4
-            >>> (:[]) >>> (read :: String -> Int)
-
-setxkbmap :: IO [T.Text]
-setxkbmap = fmap parse output
-    where
-        output :: IO T.Text
-        output = shelly . silently $ run "setxkbmap" ["-query"]
-
-        parse :: T.Text -> [T.Text]
-        parse = T.lines
-            >>> filter (T.isInfixOf "layout") >>> head
-            >>> T.words >>> (!! 1)
-            >>> T.splitOn ","
-
--- % xset -q | grep -A 0 'LED' | cut -c63-63
--- % setxkbmap -query | grep layout | tr -s ' ' | cut -d ' ' -f 2
+foreign import ccall "exp" c_exp :: Double -> Double
